d4883cfa84c6e920c499466f51bc5442fa57190d
crazcalm <crazcalm@gmail.com>
2016-03-29 23:04:49 -0400

    Part 1 of the user list functionality
    I laid out the basics needed for the user list to update when
    the client receives the user list from the server, but I need to
    add a client setting to the server so that the server can control
    when this command is sent.

--- chat_client.py
+++ chat_client.py
@@ -2,7 +2,8 @@ import asyncio
 import threading
 import tkinter as tk
 from tkinter import ttk
-from tkinter.constants import BOTH, LEFT, X, YES
+from tkinter.constants import BOTH, LEFT, X, YES, RIGHT
+from time import sleep
 
 from chat_client.gui_parts.window import Window
 from chat_client.gui_parts.frame import Frame
@@ -10,6 +11,7 @@ from chat_client.gui_parts.nav_bar import NavMenu
 from chat_client.gui_parts.nav_bar import NavTab
 from chat_client.gui_parts.nav_bar import NavItem
 from chat_client.gui_parts.text_box import ChatBox
+from chat_client.gui_parts.text_box import UserListView
 
 
 class ThreadLoop(threading.Thread):
@@ -24,16 +26,30 @@ class ThreadLoop(threading.Thread):
 
 
 class ClientProtocol(asyncio.Protocol):
-    def __init__(self, chat_box, loop):
+    def __init__(self, chat_box, user_list, loop):
         self.chat_box = chat_box
+        self.user_list = user_list
         self.loop = loop
         self.trasport = None
 
+    def get_user_list(self):
+        sleep(1)
+        self.transport.write("/CLIENT**: USER LIST".encode())
+
     def connection_made(self, transport):
         self.transport = transport
+        self.get_user_list()
 
     def data_received(self, data):
-        self.chat_box.insert_msg("\n{}".format(data.decode()))
+        msg = data.decode()
+
+        if msg.startswith("CLIENT**:"):
+            client_info = msg.split(":", 1)[1].strip()
+            self.user_list.delete_all()
+            self.user_list.create_list(client_info.split(","))
+
+        else:
+            self.chat_box.insert_msg("\n{}".format(msg))
 
     def connection_lost(self, exc):
         self.transport.close()
@@ -62,10 +78,10 @@ def test_sending_msg_callback(variable, transport=None, loop=None,
     variable.set('')
 
 
-def connect_to_server(chat_box, address=('127.0.0.1', 3333)):
+def connect_to_server(chat_box, user_list, address=('127.0.0.1', 3333)):
     loop = asyncio.get_event_loop()
     coro = loop.create_connection(lambda: ClientProtocol(
-                chat_box, loop), '127.0.0.1', 3333)
+                chat_box, user_list, loop), '127.0.0.1', 3333)
 
     transport, protocol = loop.run_until_complete(coro)
     thread = ThreadLoop(loop)
@@ -80,7 +96,8 @@ def _disconnect(gui, transport, loop):
 
 class App:
     def __init__(self, window=Window, chatbox=ChatBox, frame=Frame,
-                 nav_menu=NavMenu, nav_item=NavItem, nav_tab=NavTab):
+                 nav_menu=NavMenu, nav_item=NavItem, nav_tab=NavTab,
+                 user_list_view=UserListView):
         # Variables that will be needed
         self.loop = None
 
@@ -99,6 +116,10 @@ class App:
         self.textbox = chatbox(self.frame1)
         self.textbox.config(state="disabled")
         self.textbox.pack(side=LEFT, expand=YES, fill=BOTH, padx=10)
+
+        # creating user list view
+        self.user_list = user_list_view(self.frame1)
+        self.user_list.pack(side=RIGHT, expand=YES, fill=BOTH, padx=10)
 
         # Testing: connect to server immediately
         self.connect_to_server()
@@ -139,7 +160,7 @@ class App:
     def connect_to_server(self):
         if not self.loop:
             self.loop, self.coro, self.transport, self.protocol,\
-                self.thread = connect_to_server(self.textbox)
+                self.thread = connect_to_server(self.textbox, self.user_list)
 
     def disconnect(self):
         _disconnect(self.gui, self.transport, self.loop)
--- chat_client/gui_parts/text_box.py
+++ chat_client/gui_parts/text_box.py
@@ -1,23 +1,23 @@
 import tkinter as tk
 from tkinter import scrolledtext
-from tkinter.constants import DISABLED, ACTIVE, NORMAL, END, YES
+from tkinter.constants import DISABLED, NORMAL, END
 
 
 class ChatBox(scrolledtext.ScrolledText):
     def __init__(self, parent, height=10, width=80, state=DISABLED,
-            word=tk.WORD):
+                 word=tk.WORD):
         super().__init__(parent, height=height, width=width)
         self.height = height
         self.width = width
         self.state = state  # create a getter/setter
-    
+
     def toggle_state(self):
         if self.state == DISABLED:
             self.state = NORMAL
         else:
             self.state = DISABLED
         self.config(state=self.state)
-    
+
     def _insert(self, msg):
         if self.state == DISABLED:
             self.toggle_state()
@@ -30,12 +30,8 @@ class ChatBox(scrolledtext.ScrolledText):
 
 
 class UserListView(ChatBox):
-    def __init__(self, parent, user_list, height=20, width=30):
+    def __init__(self, parent, height=20, width=30):
         super().__init__(parent, height=height, width=width)
-        self.user_list = user_list
-
-        # create list
-        self.create_list(self.user_list)
 
     def delete_all(self):
         if self.state == DISABLED:
@@ -45,4 +41,4 @@ class UserListView(ChatBox):
 
     def create_list(self, user_list):
         for user in user_list:
-            self._insert(user)            
+            self._insert("{}\n".format(user))
